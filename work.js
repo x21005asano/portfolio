f5f41cd6e964ebb44554086bd8298a686b50de1a
x21005asano <108639169+x21005asano@users.noreply.github.com>
2022-07-22 13:08:29 +0900

    Create work.js

---
+++ work.js
@@ -0,0 +1,147 @@
+particlesJS("particles-js",{
+    "particles":{
+      "number":{
+        "value":38,//この数値を変更すると幾何学模様の数が増減できる
+        "density":{
+          "enable":true,
+          "value_area":800
+        }
+      },
+      "color":{
+        "value":"#ffffff"//色
+      },
+      "shape":{
+        "type":"polygon",//形状はpolygonを指定
+        "stroke":{
+          "width":0,
+        },
+    "polygon":{
+      "nb_sides":3//多角形の角の数
+    },
+    "image":{
+      "width":190,
+      "height":100
+    }
+    },
+      "opacity":{
+      "value":0.664994832269074,
+      "random":false,
+      "anim":{
+        "enable":true,
+        "speed":2.2722661797524872,
+        "opacity_min":0.08115236356258881,
+        "sync":false
+      }
+      },
+      "size":{
+        "value":3,
+        "random":true,
+        "anim":{
+          "enable":false,
+          "speed":40,
+          "size_min":0.1,
+          "sync":false
+        }
+      },
+      "line_linked":{
+        "enable":true,
+        "distance":150,
+        "color":"#ffffff",
+        "opacity":0.6,
+        "width":1
+      },
+      "move":{
+        "enable":true,
+        "speed":6,//この数値を小さくするとゆっくりな動きになる
+        "direction":"none",//方向指定なし
+        "random":false,//動きはランダムにしない
+        "straight":false,//動きをとどめない
+        "out_mode":"out",//画面の外に出るように描写
+        "bounce":false,//跳ね返りなし
+        "attract":{
+          "enable":false,
+          "rotateX":600,
+          "rotateY":961.4383117143238
+        }
+      }
+    },
+    "interactivity":{
+      "detect_on":"canvas",
+      "events":{
+        "onhover":{
+          "enable":false,
+          "mode":"repulse"
+        },
+    "onclick":{
+      "enable":false
+    },
+    "resize":true
+      }
+    },
+    "retina_detect":true
+  });
+
+
+  $.scrollify({
+    section : ".box",//1ページスクロールさせたいエリアクラス名
+    scrollbars:"false",//スクロールバー表示・非表示設定
+    interstitialSection : "#header,#footer",//ヘッダーフッターを認識し、1ページスクロールさせず表示されるように設定
+    easing: "swing", // 他にもlinearやeaseOutExpoといったjQueryのeasing指定可能
+      scrollSpeed: 300, // スクロール時の速度
+    
+    //以下、ページネーション設定
+    before:function(i,panels) {
+      var ref = panels[i].attr("data-section-name");
+        $(".pagination .active").removeClass("active");
+        $(".pagination").find("a[href=\"#" + ref + "\"]").addClass("active");
+      },
+      afterRender:function() {
+        var pagination = "<ul class=\"pagination\">";
+        var activeClass = "";
+        $(".box").each(function(i) {//1ページスクロールさせたいエリアクラス名を指定
+          activeClass = "";
+          if(i===$.scrollify.currentIndex()) {
+            activeClass = "active";
+          }
+          pagination += "<li><a class=\"" + activeClass + "\" href=\"#" + $(this).attr("data-section-name") + "\"><span class=\"hover-text\">" + $(this).attr("data-section-name").charAt(0).toUpperCase() + $(this).attr("data-section-name").slice(1) + "</span></a></li>";
+        });
+        pagination += "</ul>";
+  
+        $("#box1").append(pagination);//はじめのエリアにページネーションを表示
+        $(".pagination a").on("click",$.scrollify.move);
+      }
+  
+    });
+
+
+    //スクロールした際の動きを関数でまとめる
+function PageTopAnime() {
+  var scroll = $(window).scrollTop();
+  if (scroll >= 200){//上から200pxスクロールしたら
+    $('#page-top').removeClass('DownMove');//#page-topについているDownMoveというクラス名を除く
+    $('#page-top').addClass('UpMove');//#page-topについているUpMoveというクラス名を付与
+  }else{
+    if($('#page-top').hasClass('UpMove')){//すでに#page-topにUpMoveというクラス名がついていたら
+      $('#page-top').removeClass('UpMove');//UpMoveというクラス名を除き
+      $('#page-top').addClass('DownMove');//DownMoveというクラス名を#page-topに付与
+    }
+  }
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+  PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
+});
+
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+  PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
+});
+
+// #page-topをクリックした際の設定
+$('#page-top a').click(function () {
+    $('body,html').animate({
+        scrollTop: 0//ページトップまでスクロール
+    }, 500);//ページトップスクロールの速さ。数字が大きいほど遅くなる
+    return false;//リンク自体の無効化
+});
